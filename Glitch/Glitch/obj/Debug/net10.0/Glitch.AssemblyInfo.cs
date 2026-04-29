ff5940a8f105eab74ad5b8451b0cb68b98e25aee
ShanjidulNuhin <shanjidshanjidulislam1234@gmail.com>
2026-04-30 00:48:43 +0600

    remove alart box and add back button

--- Glitch/Glitch/obj/Debug/net10.0/Glitch.AssemblyInfo.cs
+++ Glitch/Glitch/obj/Debug/net10.0/Glitch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Glitch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4920394980df90a4cd0a7523f791710fbbbd821")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c711d752568dd1719f12cbd9a6b0aa16b19cd89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Glitch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Glitch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
